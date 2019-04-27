da8db604ea5dc321688f7645e329a14ee6b1e2cf
Declan Byrne <declangbyrne1987@gmail.com>
2019-04-27 18:56:52 +0000

    Added basic google map that searches with the first 3 radio buttons

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,45 +1,203 @@
+var autocomplete;
+var map;
+var markers = [];
+var infoWindow;
+var places;
 
-$(document).ready(function(){
+// Initialise map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: {lat: 54.5260, lng: 15.2551},
+        // Map controls allowing user to zoom in/out, switch to satellite mode and view streetview images
+        mapTypeControl: true,
+        zoomControl: true,
+        streetViewControl: true
+    });
 
- load_json_data('country');
+    // Info window with place details
+    infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById('place-details')
+    });
 
- function load_json_data(id, parent_id)
- {
-  var html_code = '';
-  $.getJSON('/data/country_state_city.json', function(data){
+    // Autocomplete
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'));
+        places = new google.maps.places.PlacesService(map);
+        autocomplete.addListener('place_changed', onPlaceChanged); // Resets map to new area that has been searched
+        document.getElementById('type-accommodation').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-food').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-poi').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-atm').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-stores').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-drink').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-nightclub').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-casino').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-bowling-alley').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-shopping-mall').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-park').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-amusement-park').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-museum').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-aquarium').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-zoo').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-train-station').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-taxi-stand').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-transit-station').addEventListener('change', onPlaceChanged);
+        document.getElementById('type-subway-station').addEventListener('change', onPlaceChanged);
+}
 
-   html_code += '<option value="">Select '+id+'</option>';
-   $.each(data, function(key, value){
-    if(id == 'country')
-    {
-     if(value.parent_id == '0')
-     {
-      html_code += '<option value="'+value.id+'">'+value.name+'</option>';
-     }
+// Select autocomplete query - return details of place and zoom into area
+function onPlaceChanged() {
+    var radio = $('input[name=filter]:checked');
+    var radioId = radio.attr('id');
+    
+    if (radioId == "type-accommodation") {        
+        var place = autocomplete.getPlace();                //getPlace() retrieves place name for autocomplete query
+        if (place.geometry) {
+            map.panTo(place.geometry.location);             //Retrieves lng/lat of place
+            map.setZoom(15);
+            searchAccommodation();
+        }
+        else {
+            $('#autocomplete').attr("placeholder", "Search");
+        }
     }
-    else
-    {
-     if(value.parent_id == parent_id)
-     {
-      html_code += '<option value="'+value.id+'">'+value.name+'</option>';
-     }
+    else if (radioId == "type-food") {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            searchFoodAndDrink();
+        }
+        else {
+            $('#autocomplete').attr("placeholder", "Search");
+        }
     }
-   });
-   $('#'+id).html(html_code);
-  });
+    else if (radioId == "type-poi") {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            searchPointOfInterest();
+        }
+        else {
+            $('#autocomplete').attr("placeholder", "Search");
+        }
+    }
+}
 
- }
+// Search for accommodation
+function searchAccommodation() {
+    var search = {
+        bounds: map.getBounds(),                //getBounds() retrieves lng/lat for corners of visible area of map
+        types: ['lodging', 'campground']
+    };
+    places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {         //If Places Service is working, create marker
+            clearResult();
+            clearMarker();
+            // Create marker
+            for (var x = 0; x < results.length; x++) {
+                markers[x] = new google.maps.Marker({
+                    position: results[x].geometry.location                  //Positions marker to searched location
+                });
+                // When user clicks accommodation marker, show details
+                markers[x].placeResult = results[x];
+                google.maps.event.addListener(markers[x], 'click', showInfoWindow);
+                setTimeout(dropMarkers(x), x * 100);
+            }
+        }
+    });
+}
 
- $(document).on('change', '#country', function(){
-  var country_id = $(this).val();
-  if(country_id != '')
-  {
-   load_json_data('state', country_id);
-  }
-  else
-  {
-   $('#state').html('<option value="">Select state</option>');
-   $('#city').html('<option value="">Select city</option>');
-  }
- });
-});
+// Search for food and drink
+function searchFoodAndDrink() {
+    var search = {
+        bounds: map.getBounds(),
+        types: ['restaurant', 'bar']
+    };
+    places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            clearResult();
+            clearMarker();
+            // Create marker
+            for (var x = 0; x < results.length; x++) {
+                markers[x] = new google.maps.Marker({
+                    position: results[x].geometry.location
+                });
+                // When user clicks food and drink marker, show details
+                markers[x].placeResult = results[x];
+                google.maps.event.addListener(markers[x], 'click', showInfoWindow);
+                setTimeout(dropMarkers(x), x * 100);
+            }
+        }
+    });
+}
+
+// Search for points of interest
+function searchPointOfInterest() {
+    var search = {
+        bounds: map.getBounds(),
+        types: ['shopping_mall', 'night_club', 'museum', 'art_gallery', 'park', 'amusement_park']
+    };
+    places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            clearResult();
+            clearMarker();
+            // Create marker
+            for (var x = 0; x < results.length; x++) {
+                markers[x] = new google.maps.Marker({
+                    position: results[x].geometry.location
+                });
+                // When user clicks point of interest marker, show details
+                markers[x].placeResult = results[x];
+                google.maps.event.addListener(markers[x], 'click', showInfoWindow);
+                setTimeout(dropMarkers(x), x * 100);
+            }
+        }
+    });
+}
+
+// Clear markers when new radio option selected
+function clearMarker() {
+    for (var x = 0; x < markers.length; x++) {
+        if (markers[x]) {
+            markers[x].setMap(null);
+        }
+    }
+    markers = [];
+}
+
+// Add found results to map
+function dropMarkers(x) {
+    return function() {
+        markers[x].setMap(map);
+    };
+}
+
+function clearResult() {
+    var results = document.getElementById('results');
+    while (results.childNodes[0]) {
+        results.removeChild(results.childNodes[0]);
+    }
+}
+
+// Fetch place details
+function showInfoWindow() {
+    var marker = this;
+    places.getDetails({placeId: marker.placeResult.place_id},
+        function(place, status) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                return;
+            }
+            infoWindow.open(map, marker);
+            setPlaceDetails(place);
+        });
+}
+
+// Show place details in info window on map
+function setPlaceDetails(place) {
+    document.getElementById('url').innerHTML = '<a href="' + place.url + 'target="_blank">' + place.name + '</a>';
+    document.getElementById('address').textContent = place.vicinity;
+    document.getElementById('phone').textContent = place.formatted_phone_number;
+}
